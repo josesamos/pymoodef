3c30c254641da58fe4b64e2d039f98855bb406fb
josesamos <jsamos@ugr.es>
2024-04-27 11:35:18 +0200

    build: direct access to class

--- src/pymoodef/__init__.py
+++ src/pymoodef/__init__.py
@@ -1,3 +1,6 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pymoodef")
+
+# populate package namespace
+from pymoodef.pymoodef import Questions
