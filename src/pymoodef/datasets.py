21751a9a1b4c7b8a9403192e4f0d78d6352bfb79
josesamos <jsamos@ugr.es>
2024-04-29 20:02:40 +0200

    docs: rename datasets

--- src/pymoodef/datasets.py
+++ src/pymoodef/datasets.py
@@ -14,7 +14,7 @@ def get_questions_xlsx():
 
 
 def get_questions_csv():
-    """Get path to example questions csv file.
+    """Get path to example questions csv file with "," as separator.
 
     Returns
     -------
@@ -26,15 +26,15 @@ def get_questions_csv():
     return data_file_path
 
 
-def get_questions_csv2():
-    """Get path to example questions csv2 file.
+def get_questions1_csv():
+    """Get path to example questions csv file with ";" as separator.
 
     Returns
     -------
     pathlib.PosixPath
         Path to file.
     """
-    with resources.path("pymoodef.data", "questions.csv2") as f:
+    with resources.path("pymoodef.data", "questions1.csv") as f:
         data_file_path = f
     return data_file_path
   
