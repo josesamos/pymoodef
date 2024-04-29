570aff62a81262aa8e13357e724e545c3d8fbbc4
josesamos <jsamos@ugr.es>
2024-04-29 18:26:47 +0200

    feat: call from the command line

--- src/pymoodef/__main__.py
+++ src/pymoodef/__main__.py
@@ -3,12 +3,12 @@ import sys
 
 def main():
     if len(sys.argv) == 2:
-        moodef(file = sys.argv[1])
+        res = moodef(file = sys.argv[1])
     elif len(sys.argv) == 3:
-        moodef(file = sys.argv[1], xml = sys.argv[2])
+        res = moodef(file = sys.argv[1], xml = sys.argv[2])
     else:
         raise Exception('An input file (csv or xlsx) must be indicated. We can also indicate the output xml file or it will be created in the same folder as the input file.')
-    print('XML file generated!')
+    print(f'{res} file generated!')
   
 if __name__ == "__main__":
     main()
